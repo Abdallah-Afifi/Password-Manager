b4c6789acd645f2d4d2a2966c44118190923a76b
Abdallah Fathi <114703186+AbdallahFathi22@users.noreply.github.com>
2023-04-12 06:36:12 +0200

    Create Password Manager.h

---
+++ Code/Password Manager.h
@@ -0,0 +1,18 @@
+#ifndef PASSWORDMANAGER_H
+#define PASSWORDMANAGER_H
+
+#include <string>
+
+class PasswordManager {
+
+private: 
+
+string userName;
+string Service;
+string Password;
+
+
+
+}
+
+#endif
